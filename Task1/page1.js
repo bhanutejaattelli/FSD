c498ebf87db331a715e39648fd28a8e23207439e
bhanutejaattelli <attellibhanuteja@gmail.com>
2025-01-25 14:28:04 +0530

    commited the task 1 files

---
+++ Task1/page1.js
@@ -0,0 +1,12 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    const selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+        selectedSection.style.display = 'block';
+    }
+}
+ 
